0527f2679959c9db242dc3a3e465eac88ad44bd4
Winnie Shen <huiwens@andrew.cmu.edu>
2019-11-09 21:18:35 -0500

    added ribbon config

--- src/main/java/com/microservice/project4/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/microservice/project4/inventoryservice/InventoryServiceApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {

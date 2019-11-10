bcabf5ea2dcdf6ad2d88b381f3b786ff402e0132
Winnie Shen <huiwens@andrew.cmu.edu>
2019-11-09 20:32:13 -0500

    add spring cloud config

--- src/main/java/com/microservice/project4/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/microservice/project4/inventoryservice/InventoryServiceApplication.java
@@ -2,9 +2,10 @@ package com.microservice.project4.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryServiceApplication {
 
